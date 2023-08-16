cd75c2c7dd6700ee33fc86871056abb254ed975d
Emmanuel Kpeglo <emmanuel.kpeglo@turntabl.io>
2023-08-16 14:00:46 +0000

    layout created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ApartmentContainerComponent } from './apartment-container/apartment-container.component';
+import { ApartmentComponent } from './apartment/apartment.component';
+import { SectionComponent } from './section/section.component';
+import { VideosComponent } from './videos/videos.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ContainerComponent,
+    HeaderComponent,
+    BannerComponent,
+    ApartmentContainerComponent,
+    ApartmentComponent,
+    SectionComponent,
+    VideosComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ContainerComponent]
 })
 export class AppModule { }
