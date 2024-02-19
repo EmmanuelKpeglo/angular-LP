03dc0913adc523b4a21bf3b7a1085834fecbeb1b
EmmanuelKpeglo <emmanuelkpeglo0@gmail.com>
2024-02-19 17:09:11 +0000

    first deployment to Stormer host

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -13,20 +13,8 @@ export class EmailService {
   constructor(private http: HttpClient) {}
   
   async SendEmail(data: any) {
-    emailjs.init('sAPN0vBz3TVR6xMxa');
+    emailjs.init('NHNx9PUZqvMzzogFU');
 
-    let response = await emailjs.send("service_41s3wxw","template_ypi589q", data);
+    let response = await emailjs.send("service_v3in41d","template_pkrdy2c", data);
   }
 }
-
-
-// mine
-// emailjs.init('sAPN0vBz3TVR6xMxa');
-
-// let response = await emailjs.send("service_41s3wxw","template_ypi589q", data);
-
-
-// cole
-// emailjs.init('Tv7riSWNxQSpxgz_e');
-
-// let response = await emailjs.send("service_19hlt4o","template_ygq71rd", data);
